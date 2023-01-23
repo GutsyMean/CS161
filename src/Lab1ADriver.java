6f5d03cc17a388feb3dc402439f3e3bddfa49234
GutsyMean <timothy.bogun@gmail.com>
2023-01-22 19:08:56 -0500

    first commit

---
+++ src/Lab1ADriver.java
@@ -0,0 +1,10 @@
+
+public class Lab1ADriver 
+{
+	public static void main(String[] args)
+	{
+		Rectangle box = new Rectangle();
+		Rectangle[] rectangle = new Rectangle[];
+	}
+	
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,57 @@
+
+public class Rectangle 
+{
+	int numRows;
+	int numCols;
+	
+	Boolean filled;
+	String str;
+
+	
+	public Rectangle()
+	{
+		this.numRows = 1;
+		this.numCols = 1;
+		this.filled = false;
+	}
+	public Rectangle(int numRows, int numCols)
+	{
+		this.numRows = numRows;
+		this.numCols = numCols;
+	}
+	
+	
+	
+	public int getRows()
+	{
+		return numRows;
+	}
+	
+	public int getCols()
+	{
+		return numCols;
+	}
+	
+	public void setRows(int numRows)
+	{
+		this.numRows = numRows;
+	}
+	
+	public void setCols(int numCols)
+	{
+		this.numCols = numCols;
+	}
+	
+	
+	public String toString()
+	{
+		for (int i=1; i<numRows; i++)
+		{
+			for (int j=1; j<numCols; j++)
+			{
+				str = "#";
+			}
+		}
+		return str;
+	}
+}
